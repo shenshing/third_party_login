1dc38d9c4315acccd45b0753a465c25e0f39864c
shenshing <lyshenshing20@gmail.com>
2020-06-15 02:53:31 -0400

    third-party login

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-const port = 8080;
+const port = 8081;
 app.listen(port, () => console.log(`Client listen to ${port}`));
