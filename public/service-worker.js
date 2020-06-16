cdabdfc5328cb51ab8440b3ef73883b8f324d6ea
shenshing <lyshenshing20@gmail.com>
2020-06-16 12:10:03 -0400

    change url to backend.rielcoin.com

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1c533409b85016b9652e9af104a4744.js"
+  "/precache-manifest.df43b13ff86785ff7b3feb8db5d4397a.js"
 );
 
 self.addEventListener('message', (event) => {
