1668c64fc56a86cc3329f645d4ce82ee226579fa
shenshing <lyshenshing20@gmail.com>
2020-06-16 11:52:00 -0400

    change url to backend.rielcoin.com

---
+++ public/precache-manifest.e1c533409b85016b9652e9af104a4744.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e567277bc7b584b4a9dcdfeb68ca8b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d146929086cbf56cb36",
+    "url": "/static/css/2.857f90b2.chunk.css"
+  },
+  {
+    "revision": "d5d62fe40bdda53abcdc",
+    "url": "/static/css/main.deb3cf95.chunk.css"
+  },
+  {
+    "revision": "6d146929086cbf56cb36",
+    "url": "/static/js/2.252ca0b8.chunk.js"
+  },
+  {
+    "revision": "e7a31f68a4360bf9bc1675418fde5c3e",
+    "url": "/static/js/2.252ca0b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d62fe40bdda53abcdc",
+    "url": "/static/js/main.820a65d2.chunk.js"
+  },
+  {
+    "revision": "24b486c66c3a7276b55f",
+    "url": "/static/js/runtime-main.246e8137.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "876ab531f4684e22d33640159bc74748",
+    "url": "/static/media/background3.876ab531.jpeg"
+  },
+  {
+    "revision": "ca49bba4ee5bc19ee6dc0545e112b3e8",
+    "url": "/static/media/bars.ca49bba4.svg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "37426e5d70c6ee8934bd3e563a8dba80",
+    "url": "/static/media/right-answer2.37426e5d.mp3"
+  },
+  {
+    "revision": "66615ba47ca38e6a4329077f240205ee",
+    "url": "/static/media/wrong-answer2.66615ba4.mp3"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.201ee6c21d536ebdc6ff560ed0548a4d.js"
+  "/precache-manifest.e1c533409b85016b9652e9af104a4744.js"
 );
 
 self.addEventListener('message', (event) => {
